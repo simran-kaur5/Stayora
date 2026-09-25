bee9384c5666ae63997de7d6f0ab60761f90f7c3
Simranjit Kaur <amarjitsohal769@gmail.com>
2026-09-25 17:37:41 +0530

    Added a new route

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -7,7 +7,7 @@ module.exports.createReview = async(req,res)=>{
 
     let reviewText = newReview.comment
     
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("https://stayora-sentiments.onrender.com/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
